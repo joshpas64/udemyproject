8a9c4130c2b9750eac8fcb9581a441d7f21643a3
joshpas64 <joshua.pascascio@gmail.com>
2018-06-09 17:16:12 -0700

    Admin Master Page Setup

---
+++ database/migrations/2018_06_09_205841_add_columns_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->integer('role_id')->index()->unsigned()->nullable();
+            $table->integer('is_active')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('role_id');
+            $table->dropColumn('is_active');
+        });
+    }
+}
